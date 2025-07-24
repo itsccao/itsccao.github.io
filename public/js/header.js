b923bd619ef8804940223c89b07a2cd9a23e0cd3
takai24 <takai1124@proton.me>
2025-07-24 10:15:40 +0700

    .

---
+++ public/js/header.js
@@ -0,0 +1,7 @@
+document.getElementById("page-header-menu").addEventListener("mouseover", () => {
+    document.getElementById("page-header-menu-container").style.display = "block";
+});
+
+document.getElementById("page-header-menu").addEventListener("mouseleave", () => {
+    document.getElementById("page-header-menu-container").style.display = "none";
+});
